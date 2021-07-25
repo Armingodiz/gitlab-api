f39b91d2485c78e790ae18df854d9a9741bc62fb
ArminGodiz <armingodarzi1380@gmail.com>
2021-07-25 16:19:44 +0430

    cache interface added

--- src/cache/cache.go
+++ src/cache/cache.go
@@ -1,1 +1,20 @@
 package cache
+
+
+type Cache  interface {
+	Set(string,string) error
+	Get(string) (string,error)
+}
+
+
+/*func GetNewCache(port int)Cache {
+	if port==-1{
+		return inMemoryCache{
+			mcache := make(map[string]string)
+		}
+	}else{
+		return GetRedisCache(port)
+	}
+}*/
+
+
