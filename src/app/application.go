8d4b59f02febee34b0c210832a591dac4e227d58
ArminGodiz <armingodarzi1380@gmail.com>
2021-07-25 16:13:24 +0430

    app layer implemented

--- src/app/application.go
+++ src/app/application.go
@@ -1,1 +1,19 @@
 package app
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	// router will be in charge of creating different goRoutines for different request we want to handle .
+	router = gin.Default()
+)
+
+func StartApplication() {
+	//mapUrls()
+	// for cloud to hit it and understand our web server is still running
+	err := router.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
--- src/app/routes.go
+++ src/app/routes.go
@@ -1,1 +1,7 @@
 package app
+
+func mapUrls() {
+	//	router.GET("/ping", )
+	//	router.POST("/token", )
+	//	router.GET("/projects/:id/repository_tree",)
+}
